960610fdb559c711e1bbd9f0da6612defa5a5399
Mark Weerheim <theDutchFlamingo@gmail.com>
2018-11-19 11:34:01 +0100

    Added new ShiftFinder class

--- src/Main.java
+++ src/Main.java
@@ -5,80 +5,29 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    static BufferedImage image1;
-    static BufferedImage image2;
-    static int CALCULATION_ACCELERATION_MULTIPLIER = 80;
-    static int c[] = {0, 0, 0, 0};
-
     public static void main(String[] args){
+        BufferedImage image1;
+        BufferedImage image2;
+
         try {
             image1 = ImageIO.read(new File(args[0]));
             image2 = ImageIO.read(new File(args[1]));
             System.out.println("images had been loaded");
         } catch (IOException e) {
             e.printStackTrace();
+            return;
         }
         long beginCalculationtime = System.currentTimeMillis();
 
-        int bestXShift = 0;
-        int bestYShift = 0;
-        int maxSum = 0;
+        int[] shifts = ShiftFinder.bestShifts(image1, image2);
 
-        WritableRaster im1 = image1.getRaster();
-        WritableRaster im2 = image2.getRaster();
-
-
-        //for(int yShift = -im2.getHeight() + 1; yShift < im1.getHeight(); yShift++){
-        for(int yShift = 130; yShift < im1.getHeight()/4; yShift++){
-        //int yShift = 0;
-            System.out.println("yShift: " + yShift);
-            for(int xShift = -im2.getWidth() + 1; xShift < im1.getWidth(); xShift++){
-
-                int sum = getCrossCorrelationHorizontal(im1, im2, xShift, yShift) + getCrossCorrelationVertical(im1, im2, xShift, yShift);
-                if(sum > maxSum){
-                    maxSum = sum;
-                    bestXShift = xShift;
-                    bestYShift = yShift;
-                    //System.out.println("sum: " + sum);
-                    //System.out.println(sum);
-
-                }
-            }
-        }
-
+        int bestXShift = shifts[0];
+        int bestYShift = shifts[1];
 
         System.out.println("Best shift: " + bestXShift + " x, " + bestYShift + " y");
         System.out.println("Calculation time: " + (System.currentTimeMillis() - beginCalculationtime + " ms"));
     }
 
-    static int getCrossCorrelationHorizontal(WritableRaster im1, WritableRaster im2, int xShift, int yShift){
-        //int maxSum = 0;
-        //int bestShift = 0;
-        int sum = 0;
-        for(int y = Math.max(0, yShift); y < Math.min(im1.getHeight() - 1, im2.getHeight() - 1 + yShift); y += CALCULATION_ACCELERATION_MULTIPLIER){
-            //correlation between lines
 
-            for (int x = Math.max(0, xShift); x < Math.min(im1.getWidth() - 1, im2.getWidth() - 1 + xShift); x++) {
-                sum +=  im1.getPixel(x, y, c)[2] * im2.getPixel(x - xShift, y, c)[2];
-            }
-        }
-        return sum;
-    }
-
-    static int getCrossCorrelationVertical(WritableRaster im1, WritableRaster im2, int xShift, int yShift){
-        //int maxSum = 0;
-        //int bestShift = 0;
-        int sum = 0;
-        for (int x = Math.max(0, xShift); x < Math.min(im1.getWidth() - 1, im2.getWidth() - 1 + xShift); x += CALCULATION_ACCELERATION_MULTIPLIER) {
-            //correlation between koloms
-            int sumUntil = Math.min(im1.getHeight() - 1, im2.getHeight() - 1 + yShift);
-
-
-            for (int y = Math.max(0, yShift); y < sumUntil; y++) {
-                sum += im1.getPixel(x, y, c)[2] * im2.getPixel(x, y - yShift, c)[2];
-            }
-        }
-        return sum;
-    }
 
 }
---
+++ src/ShiftFinder.java
@@ -0,0 +1,69 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+
+public class ShiftFinder {
+    static BufferedImage image1;
+    static BufferedImage image2;
+    static int CALCULATION_ACCELERATION_MULTIPLIER = 80;
+    static int c[] = {0, 0, 0, 0};
+
+    static int[] bestShifts(BufferedImage i1, BufferedImage i2) {
+        int bestXShift = 0;
+        int bestYShift = 0;
+        int maxSum = 0;
+
+        WritableRaster im1 = i1.getRaster();
+        WritableRaster im2 = i2.getRaster();
+
+        //for(int yShift = -im2.getHeight() + 1; yShift < im1.getHeight(); yShift++){
+        for(int yShift = 130; yShift < im1.getHeight()/4; yShift++){
+            //int yShift = 0;
+            System.out.println("yShift: " + yShift);
+            for(int xShift = -im2.getWidth() + 1; xShift < im1.getWidth(); xShift++){
+
+                int sum = ShiftFinder.getCrossCorrelationHorizontal(im1, im2, xShift, yShift) +
+                        ShiftFinder.getCrossCorrelationVertical(im1, im2, xShift, yShift);
+                if(sum > maxSum){
+                    maxSum = sum;
+                    bestXShift = xShift;
+                    bestYShift = yShift;
+                    //System.out.println("sum: " + sum);
+                    //System.out.println(sum);
+
+                }
+            }
+        }
+
+        return new int[] {bestXShift, bestYShift};
+    }
+
+    static int getCrossCorrelationHorizontal(WritableRaster im1, WritableRaster im2, int xShift, int yShift){
+        //int maxSum = 0;
+        //int bestShift = 0;
+        int sum = 0;
+        for(int y = Math.max(0, yShift); y < Math.min(im1.getHeight() - 1, im2.getHeight() - 1 + yShift); y += CALCULATION_ACCELERATION_MULTIPLIER){
+            //correlation between lines
+
+            for (int x = Math.max(0, xShift); x < Math.min(im1.getWidth() - 1, im2.getWidth() - 1 + xShift); x++) {
+                sum +=  im1.getPixel(x, y, c)[2] * im2.getPixel(x - xShift, y, c)[2];
+            }
+        }
+        return sum;
+    }
+
+    static int getCrossCorrelationVertical(WritableRaster im1, WritableRaster im2, int xShift, int yShift){
+        //int maxSum = 0;
+        //int bestShift = 0;
+        int sum = 0;
+        for (int x = Math.max(0, xShift); x < Math.min(im1.getWidth() - 1, im2.getWidth() - 1 + xShift); x += CALCULATION_ACCELERATION_MULTIPLIER) {
+            //correlation between koloms
+            int sumUntil = Math.min(im1.getHeight() - 1, im2.getHeight() - 1 + yShift);
+
+
+            for (int y = Math.max(0, yShift); y < sumUntil; y++) {
+                sum += im1.getPixel(x, y, c)[2] * im2.getPixel(x, y - yShift, c)[2];
+            }
+        }
+        return sum;
+    }
+}
